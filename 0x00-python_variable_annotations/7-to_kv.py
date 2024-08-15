e066700b8ae46e66fe58891bd120b3fec4051b21
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 19:35:46 +0200

    Reducing the amount of lines in the code

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 """A function that takes a string, float or int and returns a tuple"""
-
-
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
-    """
-    Returns a tuple where:
-    k: str,
-    v: [int, float]
-    """
+    """Returns a tuple from the str, int and float"""
     return k, v ** 2
