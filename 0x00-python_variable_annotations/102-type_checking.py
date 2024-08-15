da3abb64f2746619e572bc51e0f09d72a7d127cc
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 20:25:10 +0200

    Adding the corrected annotations for 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,21 @@
-Success: no issues found in 1 source file
+#!/usr/bin/env python3
+"""Using mypy to validate the piece of code"""
+
+from typing import Tuple, List
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Correcting annotations"""
+    zoomed_in: List = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+array: Tuple = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
+
+print(zoom_array.__annotations__)
+
