578cda81c3c28615ba51e6ccab1658ef8d30379b
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 18:49:34 +0200

    Trying to fix the code

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -8,7 +8,7 @@ returns the sum(float)
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Returns the sum of floats and ints
     """
