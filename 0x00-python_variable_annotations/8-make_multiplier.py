e52db5a6683d764d788213196abc7ea4df342ed4
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 19:56:49 +0200

    Fixing the pycodestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
-"""A function that takes a float and returns a function that multiplies it by another float."""
+"""
+A function that takes a float and
+returns a function that multiplies it by another float.
+"""
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    Returns a function that multiplies a given float by the specified multiplier.
+    Returns a function that multiplies a given float by specified multiplier.
     """
     def multiplier_func(x: float) -> float:
-        """Returns the result of multiplying the given float by the multiplier."""
+        """
+        Returns the result of multiplying the given float by the multiplier.
+        """
         return multiplier * x
 
     return multiplier_func
