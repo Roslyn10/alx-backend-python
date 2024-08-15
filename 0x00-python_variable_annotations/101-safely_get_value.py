96a51f5754e4d259a4b5c7051f228e72be82fb45
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 20:49:29 +0200

    Fixing the pycodestyle

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Using type annotations for the function"""
+
+from typing import TypeVar, Mapping, Any, Union, Optional
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping[Any, Any], key: Any,
+                     default: Optional[T] = None) -> Union[Any, T]:
+    """
+    Returns the value from a dictionary using a specified key,
+    or a default value if the key is not present.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
