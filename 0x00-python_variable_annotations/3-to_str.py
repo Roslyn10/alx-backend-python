606aa0ec0516d32e3a7be43816aecfa2f5eae564
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 18:23:52 +0200

    Returning a function that takes a float and returns a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A function that takes a float and returns a string"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float and returns a string
+    """
+    return str(n)
