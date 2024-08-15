95e10e9e8ebdae88208d89346a838598033ef3b9
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 19:37:36 +0200

    Fixing the tuple to Tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple from the str, int and float"""
     return k, v ** 2
