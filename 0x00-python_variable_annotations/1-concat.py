50e62d95fbc90172d054f8f3ae1f225f92a6d66c
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 18:02:42 +0200

    Adding the code that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A function that concatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenated string"""
+
+    return str1 + str2
