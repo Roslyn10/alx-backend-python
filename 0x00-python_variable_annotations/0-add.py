34957bfaf38ae7c50f7bea925b0dc0ddce8590b4
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-15 17:54:18 +0200

    Adding the add function that takes two floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A function that adds two floats together"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns the sum of two floats
+    """
+    return a + b
