aed0f236f3dadc0fddb305f5f2d144964a077ab3
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-19 15:01:56 +0200

    Adding the code for 3 and fixing the spelling error in the import from task 0, and fixing the documentation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 """An async function that takes an integer and returns asyncio.Task"""
 import asyncio
+from typing import Type
 
 
-wait_random = __import__('0-basic_async-syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Returns the asyncio.Task
+    Takes an integer max_delay and returns an asyncio.Task
+    that wraps the wait_random coroutine.
+
+    Args:
+        max_delay (int): The maximum delay in seconds for wait_random.
+
+    Returns:
+        asyncio.Task[float]: An asyncio.Task that will complete with
+                             the result of wait_random.
     """
     return asyncio.create_task(wait_random(max_delay))
