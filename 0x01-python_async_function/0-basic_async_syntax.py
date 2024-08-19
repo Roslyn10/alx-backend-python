c62d3eb75e4758ef2c28de80315e6ba24b98897d
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-19 14:17:41 +0200

    Adding the int function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Returns a random float between 0 and max_delay
     using async and random
