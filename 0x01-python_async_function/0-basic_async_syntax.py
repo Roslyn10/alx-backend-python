94e4fdf9e3faba1343447e3f5b61d2f196f45ef3
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-19 14:12:00 +0200

    fixing the pycodestyle and adding documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,15 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
+""" An async function that returns a random float after a random time delay"""
 
 import asyncio
 import random
 
+
 async def wait_random(max_delay=10):
+    """
+    Returns a random float between 0 and max_delay
+    using async and random
+    """
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
-    return random_delay 
+    return random_delay
