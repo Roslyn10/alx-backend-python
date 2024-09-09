645367a0ede293557f97c02da5ae149a523e5f78
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-09 12:45:25 +0200

    Adding the parameterized map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,9 +18,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map with various inputs"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+        ])
     def test_access_nested_map_exception(self, nested_map, path, expected):
         """Test access_nested_map while raising an exception"""
-        with self.assertRaises(exception):
+        with self.assertRaises(excepted):
             access_nested_map(nested_map, path)
 
 
