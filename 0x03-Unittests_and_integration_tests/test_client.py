dce5137f6171d81808ab8ed3ce9bb19bfc0e2d34
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-09 20:12:12 +0200

    Adding the section that tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,6 +20,18 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test_response, mock_org.return_value)
         mock_org.assert_called_once()
 
+    def test_public_repos_url(self):
+        """test the property of a public repos url"""
+        with patch("GithubOrgClient.org", new_callable=PropertyMock,) as mock_org:
+            mock_org.return_value = {
+                    'repos_url': "https://api.github.com/users/google/repos",
+                    }
+            self.assertEqual(
+                    GithubOrgClient("google")._public_repos_url,
+                    "https://api.github.com/users/google/repos",
+                    )
+
+
 
 if __name__ == '__main__':
     unittest.main()
