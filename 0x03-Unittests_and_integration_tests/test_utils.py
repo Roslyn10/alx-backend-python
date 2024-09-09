4c324c2c4cbe17cd0679c230f9f1ad96dbdad7ee
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-09 14:33:26 +0200

    Adding the test for json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +27,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map while raising an exception"""
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """A class that tests the JSon of the nested map"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    def test_get_json(self, test_url, test_payload):
+        """Test get_json function output with different URLs and payloads"""
+        attributes = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attributes)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
+
 
 
 if __name__ == '__main__':
